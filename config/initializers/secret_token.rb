387bb633996a3ecce561b50b1cd1d37bdb85c02d
prabhu <prabhu@ubuntu.ubuntu-domain>
2012-09-06 19:10:29 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apollo::Application.config.secret_token = '1219cffcac478ef5bc3f3aeafb7fd25dca27f589edb08b36f79a1317f091d49da29f69d924edee8a708c7ab4a8a035ce8597e625cbbc848710e7b56e276321a8'
